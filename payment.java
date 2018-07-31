99025fb12e0bcfeca173345abec16fc90495d80f
mano8888 <manukambakam.k@gmail.com>
2018-07-31 18:45:22 +0530

    create a new payment.java file

---
+++ payment.java
@@ -0,0 +1,30 @@
+we need to build the process about pro
+ we need to follow these type of steps
+
+ '
+
+
+
+ how to create a new pro in eclipse
+
+
+
+ first we need to open the class 
+ after that we need to test the junit case
+
+
+
+ after that open the pom.xml
+  
+
+
+ and apply the dependencies in pom.xml file
+
+
+ pom.xml means 
+
+
+ group id
+ artifact id
+ version
+
